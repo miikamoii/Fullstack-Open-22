d95d43c987b60ad405f8908716b04ad25bf4148c
miikamoii <miimoi@hotmail.com>
2022-06-09 12:38:02 +0300

    Tehtävä 1.2

--- osa_1/kurssitiedot/src/App.js
+++ osa_1/kurssitiedot/src/App.js
@@ -15,12 +15,21 @@ const App = () => {
     )
   }
 
+  const Part = (props) => {
+
+    return (
+      <p>{props.part} {props.exercises}</p>
+    )
+  }
+
   const Content = () => {
     return (
     <div>
-      <p>{part1} {exercises1}</p>
-      <p>{part2} {exercises2}</p>
-      <p>{part3} {exercises3}</p>
+      <div>
+        <Part part={part1} exercises={exercises1}/>
+        <Part part={part2} exercises={exercises2}/>
+        <Part part={part3} exercises={exercises3}/>
+      </div>
     </div>
     )
   }
