5f7a04c656aa2c7a2ba22d21a53e19e8d3930479
miikamoii <miimoi@hotmail.com>
2022-06-22 16:00:56 +0300

    kurssitiedot v.2.1

--- osa_2/kurssitiedot/src/App.js
+++ osa_2/kurssitiedot/src/App.js
@@ -32,9 +32,10 @@ const App = () => {
     const Content = ({ parts }) => {
       return(
         <div>
-            <p>{parts[0].name} {parts[0].exercises}</p>
-            <p>{parts[1].name} {parts[1].exercises}</p>
-            <p>{parts[2].name} {parts[2].exercises}</p>
+          {course.parts.map(part => 
+          <p key={part.id}>
+            {part.name} {part.exercises}
+          </p>)}
         </div>
       )
     }
