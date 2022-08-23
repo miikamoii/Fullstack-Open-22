40cf1711b611e6cac46e86cc228eec2cd1326e81
miikamoii <miimoi@hotmail.com>
2022-08-23 15:45:46 +0300

    puhelinluettelo_backend v.3.9

--- osa_3/puhelinluettelo_backend/index.js
+++ osa_3/puhelinluettelo_backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const morgan = require("morgan")
+const cors = require("cors")
 const app = express()
 
 morgan.token("body", req => {
@@ -7,6 +8,7 @@ morgan.token("body", req => {
 })
 
 app.use(express.json())
+app.use(cors())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
 
 //"tiny"
