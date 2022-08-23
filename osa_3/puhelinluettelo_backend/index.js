d064f489858c3894c11835cdd152b80a280de33e
miikamoii <miimoi@hotmail.com>
2022-08-23 16:32:12 +0300

    puhelinluettelo_backend v.3.11 (fullstack)

--- osa_3/puhelinluettelo_backend/index.js
+++ osa_3/puhelinluettelo_backend/index.js
@@ -7,6 +7,7 @@ morgan.token("body", req => {
     return JSON.stringify(req.body)
 })
 
+app.use(express.static("build"))
 app.use(express.json())
 app.use(cors())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
