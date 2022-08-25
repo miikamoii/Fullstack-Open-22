b20cdd804c00065ec2f22defdcd0eda68c335df7
miikamoii <miimoi@hotmail.com>
2022-08-25 16:08:26 +0300

    3.16: puhelinluettelo ja tietokanta

--- osa_3/puhelinluettelo_backend/index.js
+++ osa_3/puhelinluettelo_backend/index.js
@@ -37,18 +37,20 @@ app.get("/info", (req, res) => {
         )
 })
 
-app.get('/api/persons/:id', (request, response, next) => {  Person.findById(request.params.id)
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
     .then(person => {
-      if (person) {
-        response.json(person)
-      } else {
-        response.status(404).end()
-      }
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
     })
-    .catch(error => next(error))})
+    .catch(error => next(error))
+})
 
 app.delete("/api/persons/:id", (req, res, next) => {
-    
+
     Person.findByIdAndRemove(req.params.id)
         .then(result => {
             res.status(204).end()
@@ -64,7 +66,7 @@ app.post("/api/persons", (req, res) => {
         return res.status(400).json({
             error: "name or number missing"
         })
-    } 
+    }
 
     Person.find({}).then(persons => {
         persons.forEach(person => {
@@ -76,14 +78,6 @@ app.post("/api/persons", (req, res) => {
         })
     })
 
-    /* persons.forEach(person => {
-        if (person.name === body.name) {
-            return res.status(400).json({
-                error: "name is already in use"
-            })
-        }
-    }) */
-
     const person = new Person({
         name: body.name,
         number: body.number
@@ -94,6 +88,39 @@ app.post("/api/persons", (req, res) => {
         console.log("Person saved to MongoDB")
     })
 })
+
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+        .then(updatedPerson => {
+            res.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
